12e1b0a227c2aac635c343181575f096e1f83d7b
Roe <306473061@qq.com>
2022-01-14 16:03:06 +0800

    up

---
+++ 正则.py
@@ -0,0 +1,18 @@
+#正则表达式匹配某个文本模式，但是它找到的是模式的最长可能匹配（因为是贪婪匹配 ）。 而你想修改它变成查找最短的可能匹配。
+
+# import re
+# text2 = 'Computer says "no." Phone says "yes."'
+# str_pat=re.compile(r'"(.*)"')   # 匹配引号包裹的任意文本，并编译
+# print(str_pat.findall(text2))   #['no." Phone says "yes.'],并不是我们想要的['no.', 'yes.']，由于正则表达式* 匹配0到人一多次，是贪婪匹配
+# # 解决方式
+# new_str_pat=re.compile(r'"(.*?)"')
+# print(new_str_pat.findall(text2))  # ['no.', 'yes.']
+
+
+#布尔判断
+str = '2/2/1/1/1'
+str1 = None
+if bool(str1) == True:
+    print('成功')
+else:
+    print('Flase')
---
+++ 随机图api源码.py
@@ -0,0 +1,31 @@
+﻿#https://www.wrnxr.cn/456.html教程
+import json
+from random import randint
+import requests
+import time
+from flask import Flask, make_response, request
+#wrnxr.cn仙人小站欢迎您的光临
+app = Flask(__name__)
+
+img_path = str(r'E:/二次元图包20652张/P4-5467/A/')#这里是图片包的路径，改成你自己的
+
+
+@app.route('/', methods=['GET'])
+def display_img():
+    fn = randint(1, 2000)#这里(1,2000)是图片ID范围
+    filename = (str(fn) + '.jpg')
+    image_data = open(img_path + filename, "rb").read()
+    response = make_response(image_data)
+    response.headers['Content-Type'] = 'image/jpg'
+    time_now=(time.strftime('%Y-%m-%d %H:%M:%S'))
+    result = time_now + '  请求的图片ID：' + str(fn)+ ' \n'
+    print(time_now + '  请求的图片ID：' + str(fn) )
+    write_log = open('log.txt', mode='a+')
+    write_log.write(result.__str__())
+    write_log.close()
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
+
