6b6fd91bc3233fac2f92cb69c4c9c6e1134237c1
Roe <306473061@qq.com>
2022-03-21 11:05:13 +0800

    Update 获取pin.py

--- 获取pin.py
+++ 获取pin.py
@@ -1,12 +1,15 @@
 import os
+
 def main():
     print(get_cookie())
 
 def get_cookie():
+    pin_list=[]
     cookie_list= os.environ['JD_COOKIE'].split('&')
     for ck in cookie_list:
         pin=ck.split(';')[0]
-        return pin
+        pin_list.append(pin)
+    return pin_list
 
 
 if __name__ == '__main__':
